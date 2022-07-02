994f6cca46d4c4d67954ce539a6799d24f2efbb6
AJ <ajmsd6@gmail.com>
2022-07-01 21:50:58 -0700

    initial commit

---
+++ katch-frontend/src/Home.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Typography from "@mui/material/Typography"
+
+export default function Home () {
+    return (
+        <Typography>Hello Katch</Typography>
+    )
+}
