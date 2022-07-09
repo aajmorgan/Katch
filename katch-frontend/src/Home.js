cd25fd7c8127e884cc8de4e82acf821b6a1e9ac2
AJ <ajmsd6@gmail.com>
2022-07-08 21:58:36 -0700

    further work

--- katch-frontend/src/Home.js
+++ katch-frontend/src/Home.js
@@ -1,8 +1,48 @@
-import React from "react"
-import Typography from "@mui/material/Typography"
+import React, { useState } from "react"
+import { Button, Box, Typography, TextField } from "@mui/material/"
 
 export default function Home () {
+
+    const [email, setEmail] = useState("");
+    const [show, setShow] = useState("");
+
+    const Navbar = () => {
+        return (
+            <Box sx={{
+                width:"100vw",
+                height:"10vh",
+                bgcolor:"black",
+                color:"white",
+                display:"flex",
+                justifyContent:"center",
+                alignItems:"center"
+            }}>
+                <Typography fontSize={30}>K A T C H</Typography>
+            </Box>
+        )
+    }
+
     return (
-        <Typography>Hello Katch</Typography>
+        <Box>
+            <Navbar />
+            <Box 
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                height="75vh"                
+            >
+                <Typography mr={2}>Enter Email: </Typography>
+                <TextField 
+                    size="small"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <Button 
+                    variant="contained"
+                    onClick={() => setShow(email)}
+                >Submit</Button>
+            </Box>
+            <Typography>{show}</Typography>
+        </Box>
     )
 }
